19752ce2064210d10f50871eaedda21ba6f5ff56
Matt Gabor <mattgabor@gmail.com>
2020-08-23 22:21:13 -0700

    removed unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
     'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-contentful`,
